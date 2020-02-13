a237392fbb29e619bfa973c5819e00885341b2fe
heiligbasil <basilief@yahoo.com>
2020-02-12 16:48:26 -0800

    Fill in command logic inside of a While loop

--- src/adv.py
+++ src/adv.py
@@ -1,3 +1,4 @@
+import sys
 import textwrap
 from room import Room
 from player import Player
@@ -39,7 +40,6 @@ room['treasure'].s_to = room['narrow']
 # Main
 #
 
-
 # Make a new player object that is currently in the 'outside' room.
 player: Player = Player("Kowalksi", room['outside'])
 
@@ -55,6 +55,39 @@ player: Player = Player("Kowalksi", room['outside'])
 #
 # If the user enters "q", quit the game.
 
-print(f"{player.name}'s current location is {player.current_room.name}")
-print(textwrap.TextWrapper(width=50).fill(player.current_room.description))
-command: chr = input("== Where do you want to move? == (n/s/e/w) > ")
+
+while True:
+    print()
+    print(f"{player.name}'s current location is {player.current_room.name}.")
+    print(textwrap.TextWrapper(width=50).fill(player.current_room.description))
+    command: chr = input("== Where do you want to move? == (n/s/e/w) > ")
+    command = command.upper()
+    if command == 'N':
+        if player.current_room.n_to is None:
+            print(f'{player.name} cannot move North.')
+        else:
+            print(f'{player.name} has moved North.')
+            player.current_room = player.current_room.n_to
+    elif command == 'S':
+        if player.current_room.s_to is None:
+            print(f'{player.name} cannot move South.')
+        else:
+            print(f'{player.name} has moved South.')
+            player.current_room = player.current_room.s_to
+    elif command == 'E':
+        if player.current_room.e_to is None:
+            print(f'{player.name} cannot move East.')
+        else:
+            print(f'{player.name} has moved East.')
+            player.current_room = player.current_room.e_to
+    elif command == 'W':
+        if player.current_room.w_to is None:
+            print(f'{player.name} cannot move West.')
+        else:
+            print(f'{player.name} has moved West.')
+            player.current_room = player.current_room.w_to
+    elif command == 'Q':
+        print("Goodbye...")
+        sys.exit()
+    else:
+        print("|<>|Command misunderstood|<>|")
