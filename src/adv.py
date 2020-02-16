6f5e13dbf5a2fcb884637b7d244dc0a27548ab5f
heiligbasil <basilief@yahoo.com>
2020-02-15 21:33:43 -0800

    Add verb (Use) to REPL to combine Items Rename Room and Item lists for clarity Remove coloration on prompt to fix cursor position

--- src/adv.py
+++ src/adv.py
@@ -6,14 +6,15 @@ from item import Item, LightSource
 from colorama import Fore, Back, Style
 import colorama
 
-# https://github.com/tartley/colorama
-# Initialize the ANSI engine for Windows
+
+# Initialize the ANSI engine for Windows | https://github.com/tartley/colorama
 colorama.init(autoreset=True)
 
+
 # Declare all the rooms
-room = {
+room_list = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons.",is_illuminated=False),
+                     "North of you, the cave mount beckons."),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
@@ -32,29 +33,29 @@ earlier adventurers. The only exit is to the south."""),
 
 
 # Link rooms together
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+room_list['outside'].n_to = room_list['foyer']
+room_list['foyer'].s_to = room_list['outside']
+room_list['foyer'].n_to = room_list['overlook']
+room_list['foyer'].e_to = room_list['narrow']
+room_list['overlook'].s_to = room_list['foyer']
+room_list['narrow'].w_to = room_list['foyer']
+room_list['narrow'].n_to = room_list['treasure']
+room_list['treasure'].s_to = room_list['narrow']
 
 
 # Declare all of the items
-item = [Item('torch', 'A dry torch with only the top burnt.'),
-        Item('matchbook', 'A full book of dry intact matches.'),
-        Item('rope', 'A coiled sturdy-looking dry twisted rope of twine.'),
-        Item('dagger', 'A rusting and blunt weapon in a sheath.'),
-        LightSource("Flamer","A flaming torch provides orange flickering illumination.") ]
+item_list = [Item('torch', 'A dry torch with only the top burnt.'),
+             Item('matchbook', 'A full book of dry intact matches.'),
+             Item('rope', 'A coiled sturdy-looking dry twisted rope of twine.'),
+             Item('dagger', 'A rusting and blunt weapon in a sheath.'),
+             LightSource("Flamer", "A flaming torch provides orange flickering illumination.")]
 
 
 # Link the items to the rooms
-room['foyer'].items = [item[0]]
-room['overlook'].items = [item[1]]
-room['narrow'].items = [item[2]]
-room['treasure'].items = [item[3]]
+room_list['foyer'].items = [item_list[0]]
+room_list['overlook'].items = [item_list[1]]
+room_list['narrow'].items = [item_list[2]]
+room_list['treasure'].items = [item_list[3]]
 
 
 # Determine if there is enough light available to see
@@ -66,12 +67,24 @@ def enough_light():
     elif found_lightsource(player.items):
         return True
     return False
+
+
 # Search items for a LightSource
-def found_lightsource(these_items: Item = []):
+def found_lightsource(these_items: list):
     for each_item in these_items:
         if each_item.isinstance(LightSource):
             return True
     return False
+
+
+# Determine if a string exists in a list of items and if so return the index
+def item_index_in_list(this_list: list, text: str):
+    index_of_item: int = None
+    for item in this_list:
+        if item.name == text:
+            index_of_item = this_list.index(item)
+
+    return index_of_item
 
 
 #
@@ -80,15 +93,18 @@ def found_lightsource(these_items: Item = []):
 
 
 # Make a new player object that is currently in the 'outside' room.
-player: Player = Player(input("What's your name? ") or 'Kowalksi', room['outside'])
+player: Player = Player(input("What's your name? ")
+                        or 'Kowalksi', room_list['outside'])
 print(f'{Back.WHITE}{Fore.BLACK}Welcome, {player.name} :)')
+
 
 # REPL command loop - the game's engine
 while True:
     print(f'{Fore.BLUE}|<>| Location |<>|')
     print(f"{player.name}'s current location is {Back.MAGENTA}{player.current_room.name}{Back.RESET}.")
     if (enough_light()):
-        print(textwrap.TextWrapper(width=65).fill(player.current_room.description))
+        print(textwrap.TextWrapper(width=65).fill(
+            player.current_room.description))
         if len(player.current_room.items) > 0:
             for item in player.current_room.items:
                 print(
@@ -97,8 +113,7 @@ while True:
         print(f"{Back.BLACK}{Fore.WHITE}{Style.BRIGHT}It's pitch black!")
     print(
         f'You have {Fore.YELLOW}{player.xp}{Fore.RESET} XP and can move in a direction (n/s/e/w), get/drop/use items, show (i)nventory, or (q)uit.')
-    command: str = input(
-        f'{Fore.CYAN}|<>| What is your desire? |<>|{Back.RESET} ==> ')
+    command: str = input('|<>| What is your desire? |<>| ==> ')
     command = command.lower().strip().split()
     if len(command) == 1:
         # Single word entered; only look at the first letter
@@ -147,6 +162,32 @@ while True:
                     break
             else:
                 print(f'{Fore.CYAN}That item was not found.')
+        elif command[0] == 'use' or command[0] == 'activate':
+            # Manipulate an effect using items in the inventory
+            effected_object: str = command[-1]
+            index_of_item_1 = item_index_in_list(
+                player.items, commanded_object)
+            index_of_item_2 = item_index_in_list(player.items, effected_object)
+            if index_of_item_1 != None and index_of_item_2 != None:
+                # Verified both items exist in the player's inventory
+                if commanded_object == 'matchbook' and effected_object == 'torch':
+                    # Light the torch on fire by combining two items to birth a new one
+                    player.items.pop(index_of_item_1)
+                    player.items.pop(index_of_item_2)
+                    player.items.append(item_list[4])
+                    player.xp += 5
+                    print(
+                        f'{Fore.GREEN}Your ingenuity has produced a {Fore.YELLOW}{item_list[4].name}{Fore.RESET}.')
+                else:
+                    print(
+                        f"{Fore.MAGENTA}I don't understand how a {commanded_object} can activate a {effected_object}.")
+            else:
+                # Items specified are not in the player's inventory
+                if index_of_item_1 == None:
+                    print(
+                        f"{Fore.MAGENTA}You aren't carrying a {commanded_object}.")
+                if index_of_item_2 == None:
+                    print(f"{Fore.MAGENTA}You aren't carrying a {effected_object}.")
         else:
             # Unused command
             print(f'{Fore.MAGENTA}|<>| Command misunderstood |<>|')
